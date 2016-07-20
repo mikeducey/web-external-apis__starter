00473049fc1eb6a55a860fd16b9dd5cb4899e41f
Michael Ducey <michaeljducey@gmail.com>
2016-07-20 16:09:25 -0500

    submitting more work

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,12 +2,24 @@ MyApp.get "/" do
   erb :"home"
 end
 
+# time.strftime("%I""%p"
+# 	time and am or pm
+
 MyApp.get "/omaha" do
 	require_relative "secret.rb"
 	time = Time.new
-	omaha_weather_info = HTTParty.get('https://api.forecast.io/forecast/#{API_KEY}/41.2607,-95.9406')
+	@api_key = Day.getAPIKey("41.2607", "-95.9406")
 	@city = Day.cityName("Omaha")
-	@status = Day.currentWeatherStatus("#{omaha_weather_info["currently"]["summary"]}.")
+	@status = Day.currentWeatherStatus(@api_key)
+
+	# @hourly_temps = Day.hourly_temperatures(weather_information, time.strftime("%I""%p")
+
+	#  <% @hourly_temps.each_index do |temp, index| %> #this line will go above that hourly temp section in views
+	#  					<%- time_function_thingy(index) %> to print the dang time
+	#  	 and then put <%= temp %>   #this will go in each line for the hourly temp in views
+	#  	 it might be good to look up how to write a degree!!
+
+	@AMorPM = Day.AMOrPM(time.strftime("%p"))
 	@currentTemp = Day.currentWeatherTemp("#{omaha_weather_info["currently"]["temperature"]}.")
 	@currentDay = Day.currentDay(time.strftime("%A"))
 	@currentDayLow = Day.minTemp("#{omaha_weather_info["daily"]["data"][0]["temperatureMin"]}.")
--- app/models/cities.rb
+++ app/models/cities.rb
@@ -1,14 +1,34 @@
 require "httparty"
 require "pry"
 
+
 class Day
+
+	# This function returns the API Key, with the lat and long specified for that city
+
+	def Day.getAPIKey(lat, long)
+		require_relative "secret.rb"
+		return correctAPIKey = HTTParty.get('https://api.forecast.io/forecast/#{API_KEY}/#{lat},#{long}')
+	end
+
+	def Day.hourly_temperatures(weather_information)
+		temps = []
+		weather_information["hourly"]["data"].each do |hour|
+			temps << hour["temperature"]
+		end
+		return temps
+	end
+
+	def Day.AMOrPM(timeofday)
+		return timeofday
+	end
 
 	def Day.cityName(name)
 		return name
 	end
 
-	def Day.currentWeatherStatus(status)
-		return status
+	def Day.currentWeatherStatus(weather_information)
+		weather_information["currently"]["summary"]
 	end
 
 	def Day.currentWeatherTemp(temperature)
